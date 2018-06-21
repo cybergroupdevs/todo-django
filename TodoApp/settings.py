cf99ed0775449024956eb246aebe31bc41c16a51
Shivam Rastogi <shivam.ras304@gmail.com>
2018-06-21 04:10:11 +0000

    Changes for deploying on pythonanywhere

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's+6rdod#obbh8g1yr6&6oj648excc(2gicem5-f(9rgsj!5&^1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shivamras304.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/shivamras304/todo-django/static'
